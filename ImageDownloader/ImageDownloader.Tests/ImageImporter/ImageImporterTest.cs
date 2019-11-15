9b56c3282117142731c019fe73702bb2c2028bb6
Pavel Vishnyakov <vishnyakov.pavel@gmail.com>
2019-11-15 14:32:30 +0100

    start with tests for image importer

--- ImageDownloader/ImageDownloader.Tests/ImageImporter/ImageImporterTest.cs
+++ ImageDownloader/ImageDownloader.Tests/ImageImporter/ImageImporterTest.cs
@@ -1,39 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 using NUnit.Framework;
+using Importer = ImageImporter.ImageImporter;
 
-namespace ImageImporter.Tests.ImageDownloader
+namespace ImageImporter.Tests.ImageImporter
 {
     [TestFixture]
     public class ImageImporterTest
     {
+        private const string TestDataDirectoryPath = @"TestData\ImageImporter";
+        private string m_OutputDirectory;
+        private IImageImporter m_ImageImporter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_OutputDirectory = Path.Combine(Environment.CurrentDirectory, "Output");
+            if (Directory.Exists(m_OutputDirectory))
+            {
+                Directory.Delete(m_OutputDirectory, true);
+            }
+            Directory.CreateDirectory(m_OutputDirectory);
+            m_ImageImporter = new Importer();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Directory.Exists(m_OutputDirectory))
+            {
+                Directory.Delete(m_OutputDirectory, true);
+            }
+        }
+
         [Test]
-        public void ImportRawFileTest()
+        public void InitializeFromFileTest()
+        {
+            var configurationFile = Path.Combine(TestDataDirectoryPath, "..", "ConfigurationProvider", "correct.xml");
+            m_ImageImporter.Initialize(configurationFile);
+
+            Assert.IsNotNull(m_ImageImporter.Configuration);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ImportRawFileTest(bool initializeFromFile)
         {
             Assert.Fail("Test is not implemented yet");
         }
 
-        [Test]
-        public void ImportVideoFileTest()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ImportVideoFileTest(bool initializeFromFile)
         {
             Assert.Fail("Test is not implemented yet");
         }
 
-        [Test]
-        public void ImportNonRawFileTest()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ImportNonRawFileTest(bool initializeFromFile)
         {
             Assert.Fail("Test is not implemented yet");
         }
 
-        [Test]
-        public void ImportMiscFileTest()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ImportMiscFileTest(bool initializeFromFile)
         {
             Assert.Fail("Test is not implemented yet");
         }
 
-        [Test]
-        public void NoFileToImportTest()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void NoFileToImportTest(bool initializeFromFile)
         {
             Assert.Fail("Test is not implemented yet");
         }
---
+++ ImageDownloader/ImageDownloader.Tests/Utilities/TestUtilities.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ImageImporter.Tests.Utilities
+{
+    public static class TestUtilities
+    {
+        public static void ValidateConfiguration(
+            Configuration configuration, 
+            IEnumerable<string> expectedRawTypes, IEnumerable<string> expectedNonRawTypes, IEnumerable<string> expectedVideoTypes, 
+            string expectedDestination, string expectedPattern)
+        {
+            Assert.Multiple(() => 
+                { 
+                    Assert.IsNotNull(configuration);
+                    if (ReferenceEquals(expectedDestination,null))
+                    {
+                        Assert.IsNull(configuration.Destination);
+                    }
+                    else
+                    {
+                        Assert.IsNotNull(configuration.Destination);
+                        if (expectedDestination.Equals("*"))
+                        {
+                            Assert.IsNotEmpty(configuration.Destination);
+                        }
+                        else
+                        {
+                            Assert.AreEqual(expectedDestination, configuration.Destination);
+                        }
+                    }
+                    if (ReferenceEquals(expectedPattern, null))
+                    {
+                        Assert.IsNull(configuration.Pattern);
+                    }
+                    else
+                    {
+                        Assert.IsNotNull(configuration.Pattern);
+                        if (expectedPattern.Equals("*"))
+                        {
+                            Assert.IsNotEmpty(configuration.Pattern);
+                        }
+                        else
+                        {
+                            Assert.AreEqual(expectedPattern, configuration.Pattern);
+                        }
+                    }
+                    Assert.IsNotNull(configuration.FileTypes);
+                    CollectionAssert.AreEquivalent(expectedRawTypes ?? Array.Empty<string>(), configuration.FileTypes.RawFileTypes);
+                    CollectionAssert.AreEquivalent(expectedNonRawTypes ?? Array.Empty<string>(), configuration.FileTypes.NonRawFileTypes);
+                    CollectionAssert.AreEquivalent(expectedVideoTypes ?? Array.Empty<string>(), configuration.FileTypes.VideoFileTypes);
+                }
+            );
+        }
+    }
+}
