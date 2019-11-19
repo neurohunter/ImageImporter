0d6b1d9446cce5d361c6f2088dd344079cf49776
Pavel Vishnyakov <vishnyakov.pavel@gmail.com>
2019-11-19 11:26:41 +0100

    use creation date for misc file types

--- ImageDownloader/ImageDownloader/FileProcessor/GenericFileProcessor.cs
+++ ImageDownloader/ImageDownloader/FileProcessor/GenericFileProcessor.cs
@@ -10,7 +10,7 @@ namespace ImageImporter.FileProcessor
         /// <inheritdoc />
         public override string Process(FileInfo inputFile, FileKind fileKind, string outputDirectory)
         {
-            return CreateDestinationPath(outputDirectory, DateTime.Now.Date.ToString("yyyy_MM_dd"), fileKind.GetAttributeOfType<DescriptionAttribute>().Description, inputFile.Name);
+            return CreateDestinationPath(outputDirectory, inputFile.CreationTime.Date.ToString("yyyy_MM_dd"), fileKind.GetAttributeOfType<DescriptionAttribute>().Description, inputFile.Name);
         }
     }
 }
