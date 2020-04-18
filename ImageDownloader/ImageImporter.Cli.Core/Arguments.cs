5c7413ad3487236c6aac5f2d31a1426a5d743a7e
Pavel Vishnyakov <vishnyakov.pavel@gmail.com>
2020-04-18 22:35:13 +0200

    add netcore cli

---
+++ ImageDownloader/ImageImporter.Cli.Core/Arguments.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.CommandLine;
+using CommandLine;
+
+[assembly:InternalsVisibleTo("ImageImporter.Cli.Core.Tests")]
+namespace ImageImporter.Cli.Core
+{
+    internal class Arguments
+    {
+        public bool NoPathProvided
+        {
+            get => OutputDirectory.Equals(System.Environment.CurrentDirectory);
+        }
+
+        [Option(
+            'i',
+            "input-directory",
+            Required = true,
+            HelpText = "Input directory with image files") ]
+        public string InputDirectory { set; get; }
+        [Option(
+            'o',
+            "output-directory",
+            SetName = "options",
+            Required = false,
+            HelpText = "Directory to put files to (will be created if not present)")]
+        public string OutputDirectory { set; get; }
+        [Option(
+            'r',
+            "raw-types",
+            SetName = "options",
+            Required = false,
+            HelpText = "Extensions for RAW image types (e.g. .ext1 .ext2")]
+        public IEnumerable<string> RawFileExtensions { get; set; }
+        [Option(
+            'n',
+            "non-raw-types",
+            SetName = "options",
+            Required = false,
+            HelpText = "Extensions for non-RAW image types (e.g. .ext1 .ext2")]
+        public IEnumerable<string> NonRawFileExtensions { get; set; }
+        [Option(
+            'v',
+            "video-types",
+            SetName = "options",
+            Required = false,
+            HelpText = "Extensions for video image types (e.g. .ext1 .ext2")]
+        public IEnumerable<string> VideoFileExtensions { get; set; }
+        [Option(
+            'c',
+            "configuration-path",
+            SetName = "file",
+            Required = false,
+            HelpText = "Path to a configuration file")]
+        public string ConfigurationPath { set; get; }
+
+        public Arguments()
+        {
+            RawFileExtensions = new List<string>();
+            NonRawFileExtensions = new List<string>();
+            VideoFileExtensions = new List<string>();
+        }
+
+        /// <summary>
+        /// Parses comman-line arguments into domain object
+        /// </summary>
+        /// <param name="args">Command-line arguments as a strings array</param>
+        /// <returns>Instance of <see cref="Arguments"></returns>
+        public static bool Parse(string[] args, out Arguments processedArguments)
+        {
+            var arguments = new Arguments();
+            var parsed = true;
+            Parser.Default.ParseArguments<Arguments>(args)
+                .WithParsed(a =>
+                {
+                    arguments.ConfigurationPath = a.ConfigurationPath ?? default;
+                    arguments.InputDirectory = a.InputDirectory;
+                    arguments.NonRawFileExtensions = a.NonRawFileExtensions;
+                    arguments.OutputDirectory = a.OutputDirectory ?? default;
+                    arguments.VideoFileExtensions = a.VideoFileExtensions;
+                    arguments.RawFileExtensions = a.RawFileExtensions;
+                })
+                .WithNotParsed(a => parsed = false);
+            processedArguments = arguments;
+            return parsed;
+        }
+    }
+}
---
+++ ImageDownloader/ImageImporter.Cli.Core/Program.cs
@@ -0,0 +1,64 @@
+﻿using ShellProgressBar;
+using System;
+using System.Threading.Tasks;
+
+namespace ImageImporter.Cli.Core
+{
+    class Program
+    {
+        private static ProgressBar progressBar;
+
+        static void Main(string[] args)
+        {
+
+            if (!Arguments.Parse(args, out var arguments))
+            {
+                IImageImporter imageImporter = new ImageImporter();
+                imageImporter.ImportStarted += ImportStarted;
+                imageImporter.FileCopied += FileCopied;
+                imageImporter.FileFailed += FileFailed;
+                imageImporter.ImportFinished += ImportFinished;
+                imageImporter.Initialize(
+                    arguments.ConfigurationPath,
+                    arguments.OutputDirectory,
+                    arguments.RawFileExtensions,
+                    arguments.NonRawFileExtensions,
+                    arguments.VideoFileExtensions,
+                    string.Empty);
+                imageImporter.Import(arguments.InputDirectory);
+            }
+        }
+
+        private static void ImportStarted(object sender, ImportEventArgs e)
+        {
+            var progressBarOptions = new ProgressBarOptions
+            {
+                ProgressBarOnBottom = true,
+                ProgressCharacter = '=',
+                CollapseWhenFinished = false,
+                DisplayTimeInRealTime = true,
+                ForegroundColorDone = ConsoleColor.Green,
+                ForegroundColor = ConsoleColor.Yellow,
+            };
+            progressBar = new ProgressBar(e.NumberOfFiles, $"Importing files from {e.SourceDirectory} to {e.DestinationDirectory}", progressBarOptions);
+        }
+
+        private static void FileCopied(object sender, FileEventArgs e)
+        {
+            progressBar.Tick($"Copying {e.Filename} to {e.SubDirectory}");
+        }
+
+        private static void FileFailed(object sender, FileEventArgs e)
+        {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error when copying {e.Filename} to {e.SubDirectory}: {e.ErrorMessage}");
+            Console.ForegroundColor = previousColor;
+        }
+
+        private static void ImportFinished(object sender, ImportEventArgs e)
+        {
+            progressBar.Dispose();
+        }
+    }
+}
