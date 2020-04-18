5cb22b478d75b4976959e0b38558abb3b279e2fa
Pavel Vishnyakov <vishnyakov.pavel@gmail.com>
2020-04-18 22:53:33 +0200

    finish directory rename

---
+++ src/ImageImporter/EnumAttributeHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ImageImporter
+{
+    public static class EnumAttributeHelper
+    {
+        public static T GetAttributeOfType<T>(this Enum value) where T:Attribute
+        {
+            var enumType = value.GetType();
+            var memberInfo = enumType.GetMember(value.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+    }
+}
